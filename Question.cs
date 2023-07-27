47e85ca9c0fe85fe7ee01fae10608b0b8f250fd6
andulica <abalan91@yahoo.com>
2023-07-27 04:58:44 +0100

    Add project files.

---
+++ Question.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMakerProgram
+{
+    internal class Question
+    {
+        private String questionString;
+        private int[] answers;
+
+
+        public Question(String question, int[] answers)
+        {
+            this.questionString = question;
+            this.answers = answers;
+        }
+
+
+        public String GetQuestionString()
+        {
+            return this.questionString;
+        }
+
+        public int[] GetAnswers()
+        {
+            return this.answers;
+        }
+
+
+        public void SetQuestion(String question)
+        {
+            this.questionString = question;
+
+        }
+
+        public void SetAnswers(int[] answers)
+        {
+            this.answers = answers;
+        }
+    }
+}
---
+++ QuizLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMakerProgram
+{
+    internal class QuizLogic
+    {
+
+
+        // 1. Create a method to serialize the objects into a vile
+
+        // 2. Create a method that deserialize the objects from a file back into a list.
+
+        // 3. Randomly choose a question object from the list
+
+        // 4. Check if the answer(s) entered by the user is/are correct
+
+        // 5. 
+
+    }
+}
